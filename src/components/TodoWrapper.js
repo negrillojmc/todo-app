1f982855d826996ab435a220d7af19c99346d0bb
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-03-14 07:57:28 +0800

    changed text for the <h1> tag

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -40,7 +40,7 @@ const TodoWrapper = () => {
 
   return (
     <div className='TodoWrapper'>
-      <h1>The Avatar's Todo List</h1>
+      <h1>Today's Tasks</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo, index) => (
         todo.isEditing ? (
